7f4599f8a95677ec5d3556f948dacf9d8841bf86
Candace Sheremeta <candace.aleea@gmail.com>
2016-07-29 19:09:03 -0400

    Update ROLE_helper.py
    added more documentation

--- ROLE_helper.py
+++ ROLE_helper.py
@@ -1,4 +1,10 @@
 #!/bin/python
+# Part of the packaged rpm I created to install the program "ROLE Enrollment Helper"
+# which can be found at the Fedora COPR page:
+# https://copr-be.cloud.fedoraproject.org/results/csheremeta/role-enrollment-helper/epel-7-x86_64/00429170-role-enrollments-helper/role-enrollments-helper-1.0.0-2.el7.centos.noarch.rpm
+# (You will need to sudo yum install -y <that_link> in your terminal)
+#
+# Thanks!   - Candace Sheremeta (candace.aleea@gmail.com)
 import re
 myfile = open("/tmp/temp.csv", "r")
 data=myfile.read()
